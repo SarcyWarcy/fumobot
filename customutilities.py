bdcbc84674102ca4c0f372b4f88597dbbd243928
SarcyWarcy <dianvita004@gmail.com>
2025-01-24 03:11:02 -0800

    Added blackjack custom utilities, blackjack command, blackjack view, and added card emojis

--- customutilities.py
+++ customutilities.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
 from datetime import timedelta
 import asqlite
-import random
+import pydealer as pd
+from pydealer.const import VALUES
 
 # THIS IS ALL FOR CONVERTERS
 
@@ -20,7 +21,83 @@ class Duration(commands.Converter):
       
     return timedelta(seconds=seconds)
 
+def cardEmoji(card):
+  emojis = {
+    "Ace of Spades": "<:AceofSpades:1331377971021217802>",
+    "King of Spades": "<:KingofSpades:1331378168178802830>",
+    "Queen of Spades": "<:QueenofSpades:1331378251389472871>",
+    "Jack of Spades": "<:JackofSpades:1331378073429475398>",
+    "10 of Spades": "<:10ofSpades:1331377871385530480>",
+    "9 of Spades": "<:9ofSpades:1331377797569970278>",
+    "8 of Spades": "<:8ofSpades:1331377713851662356>",
+    "7 of Spades": "<:7ofSpades:1331377628585922660>",
+    "6 of Spades": "<:6ofSpades:1331377553629384815>",
+    "5 of Spades": "<:5ofSpades:1331377470108340254>",
+    "4 of Spades": "<:4ofSpades:1331377293515292672>",
+    "3 of Spades": "<:3ofSpades:1331377272623730780>",
+    "2 of Spades": "<:2ofSpades:1331377197105283072>",
+    "Ace of Clubs": "<:AceofClubs:1331377890075607072>",
+    "King of Clubs": "<:KingofClubs:1331378103716544603>",
+    "Queen of Clubs": "<:QueenofClubs:1331378189749981357>",
+    "Jack of Clubs": "<:JackofClubs:1331377992688992276>",
+    "10 of Clubs": "<:10ofClubs:1331377818738888814>",
+    "9 of Clubs": "<:9ofClubs:1331377738354917427>",
+    "8 of Clubs": "<:8ofClubs:1331377649913827339>",
+    "7 of Clubs": "<:7ofClubs:1331377571547578519>",
+    "6 of Clubs": "<:6ofClubs:1331377494737027225>",
+    "5 of Clubs": "<:5ofClubs:1331377416202883173>",
+    "4 of Clubs": "<:4ofClubs:1331377334057570437>",
+    "3 of Clubs": "<:3ofClubs:1331377216462000188>",
+    "2 of Clubs": "<:2ofClubs:1331377137747492884>",
+    "Ace of Hearts": "<:AceofHearts:1331377948493611110>",
+    "King of Hearts": "<:KingofHearts:1331378148675293256>",
+    "Queen of Hearts": "<:QueenofHearts:1331378227804901428>",
+    "Jack of Hearts": "<:JackofHearts:1331378041984778292>",
+    "10 of Hearts": "<:10ofHearts:1331377853366927380>",
+    "9 of Hearts": "<:9ofHearts:1331377777101770874>",
+    "8 of Hearts": "<:8ofHearts:1331377694549475439>",
+    "7 of Hearts": "<:7ofHearts:1331377611401728081>",
+    "6 of Hearts": "<:6ofHearts:1331377532284698656>",
+    "5 of Hearts": "<:5ofHearts:1331377450302832650>",
+    "4 of Hearts": "<:4ofHearts:1331377372103966842>",
+    "3 of Hearts": "<:3ofHearts:1331377257696071782>",
+    "2 of Hearts": "<:2ofHearts:1331377179224703026>",
+    "Ace of Diamonds": "<:AceofDiamonds:1331377914511626250>",
+    "King of Diamonds": "<:KingofDiamonds:1331378128924315729>",
+    "Queen of Diamonds": "<:QueenofDiamonds:1331378211120087040>",
+    "Jack of Diamonds": "<:JackofDiamonds:1331378018014199828>",
+    "10 of Diamonds": "<:10ofDiamonds:1331377837873303644>",
+    "9 of Diamonds": "<:9ofDiamonds:1331377757644390543>",
+    "8 of Diamonds": "<:8ofDiamonds:1331377677395034194>",
+    "7 of Diamonds": "<:7ofDiamonds:1331377591050833961>",
+    "6 of Diamonds": "<:6ofDiamonds:1331377515997958154>",
+    "5 of Diamonds": "<:5ofDiamonds:1331377431566618714>",
+    "4 of Diamonds": "<:4ofDiamonds:1331377355041800282>",
+    "3 of Diamonds": "<:3ofDiamonds:1331377240600084621>",
+    "2 of Diamonds": "<:2ofDiamonds:1331377157028577380>",
+  }
+
+  return f"{emojis.get(f"{card.value} of {card.suit}")}"
+
 # USEFUL FUNCTIONS
+def handValue(hand):
+  value = 0
+  aces = 0
+
+  for card in hand:
+    if card.value in ["King", "Queen", "Jack"]:
+      value += 10
+    elif card.value == "Ace":
+      value += 11
+      aces += 1
+    else:
+      value += int(card.value)
+  
+  while value > 21 and aces:
+    value -= 11
+    aces -= 1
+  
+  return value
 
 async def updateBalance(id: int, power: int):
   async with asqlite.connect("fumo.db") as db:
@@ -33,6 +110,7 @@ async def updateBalance(id: int, power: int):
       await db.commit()
 
 async def checkBalance(id: int):
+  """Checks a user's balance based on the given ID."""
   async with asqlite.connect("fumo.db") as db:
     async with db.cursor() as cursor:
       result = await cursor.execute(
--- extensions/fumocommands.py
+++ extensions/fumocommands.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ui import View, Button
 from io import BytesIO
 from discord.ext import commands
 import aiohttp
@@ -8,7 +9,7 @@ import asyncio
 import asqlite
 from datetime import timedelta, datetime
 import random
-from PIL import Image
+import pydealer as pd
 
 async def fumoDbCheck():
   async with asqlite.connect("fumo.db") as db:
@@ -21,6 +22,94 @@ async def fumoDbCheck():
       )
       """)
       await db.commit()
+
+class BlackjackView(View):
+  def __init__(self, ctx, hand, dealerHand, embed, deck, bet, pscore, timeout = 20):
+    super().__init__(timeout=timeout)
+    self.ctx = ctx
+    self.hand = hand
+    self.dealerHand = dealerHand
+    self.embed = embed
+    self.deck = deck
+    self.bet = bet
+    self.pscore = pscore
+
+    self.winnings = bet
+    self.gameRover = False
+
+  async def interaction_check(self, interaction):
+    if interaction.user.id != self.ctx.author.id:
+      await interaction.response.send_message("This button is not for you!!", ephemeral=True)
+      return False
+    else:
+      return True
+
+  async def on_timeout(self):
+    if not self.gameRover:
+      for item in self.children:
+        item.disabled= True
+
+      await self.message.edit(view=self)
+      await self.ctx.reply("You didn't respond in time! The whole game is cancelled, and fortunately, the dealer was kind enough to give you your money back.")
+  
+  @discord.ui.button(label="Hit!", style=discord.ButtonStyle.green)
+  async def hitButton(self, interaction: discord.Interaction, button: Button):
+    self.hand.add(self.deck.deal(1))
+    self.pscore = customutilities.handValue(self.hand)
+
+    self.embed = discord.Embed(
+      description=f"{f"**Oh no! You busted! You lost <:Power:1331214328774529025> {self.bet}**...\n\n" if self.pscore > 21 else ""}**Your Hand**:\n# {" ".join(customutilities.cardEmoji(card) for card in self.hand)}\nScore: {self.pscore}\n\n**Dealer's Hand**:\n# {"<:UnknownOfSuit:1331378270611964068>" if self.pscore <= 21 else customutilities.cardEmoji(self.dealerHand[0])} {" ".join(customutilities.cardEmoji(card) for card in self.dealerHand[1:])}\nScore: {customutilities.handValue(self.dealerHand) if self.pscore > 21 else customutilities.handValue(self.dealerHand[1:])}",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    self.embed.set_footer(
+      text=self.ctx.author.name,
+      icon_url=self.ctx.author.avatar.url
+    )
+
+    if self.pscore > 21:
+      await customutilities.updateBalance(self.ctx.author.id, self.bet * -1)
+      self.gameOver()
+      self.gameRover = True
+
+    await interaction.response.edit_message(embed=self.embed, view=self)
+  
+  @discord.ui.button(label="Stand!", style=discord.ButtonStyle.red)
+  async def standButton(self, interaction: discord.Interaction, button: Button):
+    dealerScore = customutilities.handValue(self.dealerHand)
+
+    while customutilities.handValue(self.dealerHand) < 17:
+      self.dealerHand.add(self.deck.deal(1))
+      dealerScore = customutilities.handValue(self.dealerHand)
+    
+    if dealerScore > self.pscore and (dealerScore < 22):
+      await customutilities.updateBalance(self.ctx.author.id, self.bet * -1)
+      self.gameOver()
+      self.gameRover = True
+    elif dealerScore == self.pscore:
+      self.gameOver()
+      self.gameRover = True
+    else:
+      await customutilities.updateBalance(self.ctx.author.id, self.winnings)
+      self.gameOver()
+      self.gameRover = True
+    
+    self.embed = discord.Embed(
+      description=f"{f"**Ahh! You lost! You reluctantly gave away <:Power:1331214328774529025> {self.bet} to the dealer!**" if dealerScore > self.pscore and (dealerScore < 22) else "**You drew with the dealer! You didn't win anything...**" if dealerScore == self.pscore else f"**Yay! The dealer {"busted" if dealerScore > 21 else "lost"}! You won <:Power:1331214328774529025> {self.winnings}**"}\n\n**Your Hand**:\n# {" ".join(customutilities.cardEmoji(card) for card in self.hand)}\nScore: {self.pscore}\n\n**Dealer's Hand**:\n# {" ".join(customutilities.cardEmoji(card) for card in self.dealerHand)}\nScore: {customutilities.handValue(self.dealerHand)}",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    self.embed.set_footer(
+      text=self.ctx.author.name,
+      icon_url=self.ctx.author.avatar.url
+    )
+
+    await interaction.response.edit_message(embed=self.embed, view=self)
+  
+  def gameOver(self):
+    self.standButton.disabled = True
+    self.hitButton.disabled = True
+
 
 class FumoCommands(commands.Cog, name="Fumo"):
   """Take care of your Fumos here in this simple pet economy game!"""
@@ -53,7 +142,7 @@ class FumoCommands(commands.Cog, name="Fumo"):
 
     if row is None:
       await ctx.reply(embed=discord.Embed(
-        description=f"**Balance**:\n<:power_item:1329068042650386518> 0",
+        description=f"**Balance**:\n<:Power:1331214328774529025> 0",
         timestamp=datetime.now(),
         color=discord.Color.purple(),
       ).set_footer(
@@ -66,7 +155,7 @@ class FumoCommands(commands.Cog, name="Fumo"):
       return
           
     balanceEmbed = discord.Embed(
-      description=f"**Balance**:\n<:power_item:1329068042650386518> {row[0]}",
+      description=f"**Balance**:\n<:Power:1331214328774529025> {row[0]}",
       timestamp=datetime.now(),
       color=discord.Color.purple(),
     )
@@ -83,7 +172,7 @@ class FumoCommands(commands.Cog, name="Fumo"):
     """Gambles your power away. You win everytime you don't roll a 6. Pays out 0.2x of your original bet."""
     await customutilities.lowBalance(ctx, power=power)
     if power <= 2:
-      raise commands.BadArgument("You cannot bet less than <:power_item:1329068042650386518> 2!")
+      raise commands.BadArgument("You cannot bet less than <:Power:1331214328774529025> 2!")
 
     diceEmojis = [
       "<:DiceOne:1331122935322382437>",
@@ -106,7 +195,7 @@ class FumoCommands(commands.Cog, name="Fumo"):
     roll = random.randint(1, 6)
     winnings = round(power * 0.2) if roll != 6 else power * -1
     rolledEmbed = discord.Embed(
-      description=f"{diceEmojis[roll - 1]} | {"Good job!" if roll != 6 else "Oh no..."} You rolled a {roll}! You {"won" if roll != 6 else "lost"} <:power_item:1329068042650386518> {winnings if roll != 6 else winnings * -1}{"!" if roll != 6 else "..."}",
+      description=f"{diceEmojis[roll - 1]} | {"Good job!" if roll != 6 else "Oh no..."} You rolled a {roll}! You {"won" if roll != 6 else "lost"} <:Power:1331214328774529025> {winnings if roll != 6 else winnings * -1}{"!" if roll != 6 else "..."}",
       timestamp=datetime.now(),
       color=discord.Color.purple()
     )
@@ -121,7 +210,38 @@ class FumoCommands(commands.Cog, name="Fumo"):
   
   @commands.command(name="blackjack", aliases=["bj"])
   async def blackjack(self, ctx, power: int = commands.parameter(description="The amount of power to be put on bet.")):
-    pass
+    """Gambles your power away in a game of Blackjack. Pays out 2x, and Blackjacks pays out 2.5x"""
+    await customutilities.lowBalance(ctx, power=power)
+    if power <= 0:
+      raise commands.BadArgument("You cannot bet less than <:Power:1331214328774529025> 0!")
+
+    bjDeck = pd.Deck()
+    bjDeck.shuffle()
+    
+    playerHand = bjDeck.deal(2)
+    dealerHand = bjDeck.deal(2)
+    
+    initialScore = customutilities.handValue(playerHand)
+    
+    bjEmbed = discord.Embed(
+      description=f"{"**BLACKJACK!**\n" if initialScore == 21 else ""}**Your Hand**:\n# {" ".join(customutilities.cardEmoji(card) for card in playerHand)}\nScore: {initialScore}\n\n**Dealer's Hand**: \n# <:UnknownOfSuit:1331378270611964068> {" ".join(customutilities.cardEmoji(card) for card in dealerHand[1:])}\nScore: {customutilities.handValue(dealerHand[1:])}",
+      timestamp=datetime.now(),
+      color=discord.Color.purple()
+    )
+    bjEmbed.set_footer(
+      text=ctx.author.name,
+      icon_url=ctx.author.avatar.url
+    )
+
+    bjMessage = await ctx.reply(embed=bjEmbed)
+
+    view = BlackjackView(ctx, playerHand, dealerHand, bjEmbed, bjDeck, power, initialScore)
+
+    if initialScore == 21:
+      view.winnings = power + int((power * 0.5))
+
+    view.message = await bjMessage.edit(embed=bjEmbed, view=view)
+
 
 async def setup(bot):
   await bot.add_cog(FumoCommands(bot))
